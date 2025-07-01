bfe68480eaa0961a4119e2172c842ca0fc87e891
Michael Jendryke <michael.jendryke@geoinsight.ai>
2025-07-01 14:54:35 +0200

    WIP: All functions needed for the the adapter work, preparing to integrate via macros

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,10 @@ fn main() {
 
     let my_app = Application::new(&args);
     let dggal = DGGAL::new(&my_app);
-    let dggrs: DGGRS = DGGRS::new(&dggal, "IVEA9R").expect("Unknown DGGRS");
+    let dggrs: DGGRS = DGGRS::new(&dggal, "IVEA3H").expect("Unknown DGGRS");
+
+    let max_depth = dggrs.getMaxDepth();
+    println!("The maximum depth of this dggrs is: \n{:?}\n\n", max_depth);
 
     let pnt: GeoPoint = GeoPoint {
         lat: 52.3,
@@ -26,13 +29,33 @@ fn main() {
     };
 
     let zone = dggrs.getZoneFromWGS84Centroid(8, &pnt);
-    println!("{:?}", zone);
+    println!("The Zone ID for the given point is: \n{:?}\n\n", zone);
 
-    let neighbors = dggrs.getZoneNeighbors(zone, *mut [i32;6]);
-    println!("{:?}", neighbors);
+    let zone_level = dggrs.getZoneLevel(zone);
+    println!("The level of that Zone is: \n{:?}\n\n", zone_level);
 
-    let kids = dggrs.getSubZones(zone, 1);
-    println!("{:?}", kids);
+    let mut nb_types: [i32; 6] = [0; 6];
+    let neighbors = dggrs.getZoneNeighbors(zone, &mut nb_types);
+    println!("The neighbors of that zone: \n{:?}\n\n", neighbors);
+
+    if zone_level + 1 > max_depth {
+        println!(
+            "Zone level: {:?} is already max depth {:?}",
+            zone_level, max_depth
+        );
+    } else {
+        let kids = dggrs.getSubZones(zone, 1);
+        println!("The children of that zone: \n{:?}\n\n", kids);
+    }
+
+    let vertices: Vec<GeoPoint> = dggrs.getZoneWGS84Vertices(zone);
+    print!("These are the vertices of the zone: \n{:?}\n\n", vertices);
+
+    let rvertices: Vec<GeoPoint> = dggrs.getZoneRefinedWGS84Vertices(zone, 0);
+    print!(
+        "These are the refined vertices of the zone: \n{:?}\n\n",
+        rvertices
+    );
 
     let ll: GeoPoint = GeoPoint {
         lat: 14.5,
@@ -43,8 +66,8 @@ fn main() {
         lon: 20.3,
     };
 
-    let mut bbox = GeoExtent { ll, ur };
-    println!("{:?}", bbox);
+    let bbox = GeoExtent { ll, ur };
+    println!("The extent of the bbox: \n{:?}\n\n", bbox);
     // let mut options = HashMap::<&str, &str>::new();
 
     // let mut exit_code: i32 = 0;
@@ -53,24 +76,51 @@ fn main() {
     //     exit_code = 1
     // }
     // println!("{:?}", bbox);
-    println!("{:?}", wholeWorld);
+    println!("The extent of the whole world: \n{:?}\n\n", wholeWorld);
 
     let zones = dggrs.listZones(2, &wholeWorld);
-    println!("{:?}", zones.len());
+    println!(
+        "The length of the array of zone IDs for the whole world: \n{:?}\n\n",
+        zones.len()
+    );
 
     let subzones = dggrs.getSubZones(zone, 5);
-    println!("{:?}", subzones.len());
+    println!(
+        "The length of the array of zone IDs of a parent zone: \n{:?}\n\n",
+        subzones.len()
+    );
+
+    use std::time::Instant;
+    let t0 = Instant::now();
+    use rayon::prelude::*;
+    let ga: Vec<_> = subzones
+        .iter() // WARN: par_iter does not work because the underlying ecere/dggal C FFI is not threat safe.
+        .map(|zone: &u64| {
+            let z: u64 = *zone;
+            //println!("{:?}", z);
+            //let my_app2 = Application::new(&args);
+            //let dggal2 = DGGAL::new(&my_app2);
+            //let dggrs2: DGGRS = DGGRS::new(&dggal2, "IVEA9R").expect("Unknown DGGRS");
+            dggrs.getZoneWGS84Vertices(z)
+        })
+        .collect();
+    println!("getZoneWGS84Verticies() took {:.2?}", t0.elapsed());
+
+    //println!("{:?}", ga);
 
     println!("here");
 
-    std::process::exit(0);
-
     let configs = vec![
         (
-            String::from("DGGRID"),
-            String::from("ISEA3H"),
-            String::from("03a000000000000000"),
+            String::from("DGGAL"),
+            String::from("IVEA3H"),
+            String::from("1297036692682743552"),
         ),
+        // (
+        //     String::from("DGGRID"),
+        //     String::from("ISEA3H"),
+        //     String::from("03a000000000000000"),
+        // ),
         // (
         //     String::from("DGGRID"),
         //     String::from("IGEO7"),
