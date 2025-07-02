693ad10b9e18555f6bd1dc998722062cb958a5a1
Daniel Loos <dloos@bgc-jena.mpg.de>
2025-07-02 16:20:03 +0200

    Revert "Add title to website"
    This reverts commit 67c7c0cdf818f64d45540c14e632c793fd0f2818.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,5 @@
 const branch = process.env.GITHUB_REF_NAME || "dev"; // fallback to main if not set
 
 export default {
-  base: `/GeoPlegma/${branch}/`,
-  title: "GeoPlegma",
+  base: "/GeoPlegma/",
 };
