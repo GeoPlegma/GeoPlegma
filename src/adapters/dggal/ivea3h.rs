12ef86cec20573932d966c5a1d85954156382eb8
Michael Jendryke <michael.jendryke@geoinsight.ai>
2025-07-04 12:22:08 +0200

    WIP: working version, now with DGGAL context and no segmentation fault at the end of main.rs

--- src/adapters/dggal/ivea3h.rs
+++ src/adapters/dggal/ivea3h.rs
@@ -10,12 +10,12 @@
 use crate::adapters::dggal::dggal::DggalAdapter;
 use crate::models::common::Zones;
 use crate::ports::dggrs::DggrsPort;
-use geo::{LineString, Point, Polygon};
+use geo::Point;
 extern crate ecrt;
-use ecrt::{Application, tokenizeWith};
+use ecrt::Application;
 extern crate dggal;
 use crate::adapters::dggal::common::{ids_to_zones, to_geo_extent, to_geo_point};
-use dggal::{DGGAL, DGGRS, GeoExtent, GeoPoint, wholeWorld};
+use dggal::{DGGAL, DGGRS};
 use std::env;
 
 pub struct Ivea3hImpl {
@@ -38,19 +38,32 @@ impl Default for Ivea3hImpl {
     }
 }
 
-fn get_dggrs() -> DGGRS {
+pub struct DGGALContext {
+    pub _app: Application,
+    pub _dggal: DGGAL,
+    pub dggrs: DGGRS,
+}
+
+fn get_dggal_context() -> DGGALContext {
     let args: Vec<String> = env::args().collect();
     let my_app = Application::new(&args);
     let dggal = DGGAL::new(&my_app);
     let dggrs: DGGRS = DGGRS::new(&dggal, "IVEA3H").expect("Unknown DGGRS");
-    dggrs
+    DGGALContext {
+        _app: my_app,
+        _dggal: dggal,
+        dggrs,
+    }
 }
 
 impl DggrsPort for Ivea3hImpl {
     fn zones_from_bbox(&self, depth: u8, densify: bool, bbox: Option<Vec<Vec<f64>>>) -> Zones {
-        let d = get_dggrs();
-        let max_depth = d.getMaxDepth();
-
+        let ctx = get_dggal_context();
+        //let args: Vec<String> = env::args().collect();
+        //let my_app = Application::new(&args);
+        //let dggal = DGGAL::new(&my_app);
+        //let dggrs: DGGRS = DGGRS::new(&dggal, "IVEA3H").expect("Unknown DGGRS");
+        let max_depth = ctx.dggrs.getMaxDepth();
         let capped_depth = if depth as i32 > max_depth {
             max_depth
         } else {
@@ -58,22 +71,28 @@ impl DggrsPort for Ivea3hImpl {
         };
 
         let zones = if let Some(b) = bbox {
-            d.listZones(capped_depth, &to_geo_extent(Some(b)))
+            ctx.dggrs.listZones(capped_depth, &to_geo_extent(Some(b)))
         } else {
-            d.listZones(capped_depth, &wholeWorld)
+            println!("here");
+            ctx.dggrs.listZones(
+                capped_depth,
+                &to_geo_extent(Some(vec![vec![-90.0, -180.0], vec![90.0, 180.0]])),
+            )
         };
 
-        ids_to_zones(d, zones)
+        ids_to_zones(ctx.dggrs, zones)
     }
     fn zone_from_point(&self, depth: u8, point: Point, densify: bool) -> Zones {
-        let d = get_dggrs();
-        let zone = d.getZoneFromWGS84Centroid(8, &to_geo_point(point));
+        let ctx = get_dggal_context();
+        let zone = ctx
+            .dggrs
+            .getZoneFromWGS84Centroid(depth as i32, &to_geo_point(point));
         let zones = vec![zone];
-        ids_to_zones(d, zones)
+        ids_to_zones(ctx.dggrs, zones)
     }
     fn zones_from_parent(&self, depth: u8, parent_zone_id: String, densify: bool) -> Zones {
-        let d = get_dggrs();
-        let max_depth = d.getMaxDepth();
+        let ctx = get_dggal_context();
+        let max_depth = ctx.dggrs.getMaxDepth();
 
         let capped_depth = if depth as i32 > max_depth {
             max_depth
@@ -82,14 +101,14 @@ impl DggrsPort for Ivea3hImpl {
         };
 
         let num: u64 = parent_zone_id.parse::<u64>().expect("Invalid u64 string"); // FIX: parent_zone_id needs to be the ZoneID enum not String
-        let zones = d.getSubZones(num, 5);
+        let zones = ctx.dggrs.getSubZones(num, 5);
 
-        ids_to_zones(d, zones)
+        ids_to_zones(ctx.dggrs, zones)
     }
     fn zone_from_id(&self, zone_id: String, densify: bool) -> Zones {
-        let d = get_dggrs();
+        let ctx = get_dggal_context();
         let num: u64 = zone_id.parse::<u64>().expect("Invalid u64 string"); // FIX: parent_zone_id needs to be the ZoneID enum not String
         let zones = vec![num];
-        ids_to_zones(d, zones)
+        ids_to_zones(ctx.dggrs, zones)
     }
 }
