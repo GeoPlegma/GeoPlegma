0e4667680f5601e1fe957bcdb763df69eb2df13e
Michael Jendryke <michael.jendryke@geoinsight.ai>
2025-07-10 09:28:05 +0200

    WIP: taking dggal-rust from git and adding Coppyright notice

--- src/adapters/h3o/h3.rs
+++ src/adapters/h3o/h3.rs
@@ -1,3 +1,12 @@
+// Copyright 2025 contributors to the GeoPlegmata project.
+// Originally authored by Michael Jendryke (GeoInsight GmbH, michael.jendryke@geoinsight.ai)
+//
+// Licenced under the Apache Licence, Version 2.0 <LICENCE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENCE-MIT or http://opensource.org/licenses/MIT>, at your
+// discretion. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use crate::adapters::h3o::common::{cells_to_zones, res};
 use crate::adapters::h3o::h3o::H3oAdapter;
 use crate::error::h3o::H3oError;
