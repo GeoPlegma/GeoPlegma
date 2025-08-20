d5c4b300f1e3cd3483b6e73e50aed8d5cf461c7e
Joao Manuel <joaocsmanuel@gmail.com>
2025-08-20 18:14:30 +0100

    Add authoring

--- dggrs/src/macros/wasm.rs
+++ dggrs/src/macros/wasm.rs
@@ -1,3 +1,13 @@
+// Copyright 2025 contributors to the GeoPlegma project.
+// Originally authored by João Manuel (GeoInsight GmbH, joao.manuel@geoinsight.ai)
+//
+// Licenced under the Apache Licence, Version 2.0 <LICENCE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENCE-MIT or http://opensource.org/licenses/MIT>, at your
+// discretion. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Macros that will be used later for wasm
 // keeping both macro separate is often more maintainable
 // and avoids implicit type guessing
 
