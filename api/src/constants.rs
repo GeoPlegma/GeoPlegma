8bb90edf9bd1cef3530ca16765ec125e2b1d3e61
mj <michael.jendryke@geoinsight.ai>
2025-11-13 16:22:49 +0100

    Upgrade to DGGAL version 0.0.6 with IVEA7H and IVEA7H_Z7 (#74)
    * added dggal-0.0.5 from dggal-rust and IVEA7H and IVEA7H_Z7
    
    * update to dggal 0.0.6
    
    * clean up

--- api/src/constants.rs
+++ api/src/constants.rs
@@ -22,7 +22,7 @@ pub fn whole_earth_bbox() -> Rect<f64> {
     )
 }
 
-pub const DGGRS_SPECS: [DggrsSpec; 9] = [
+pub const DGGRS_SPECS: [DggrsSpec; 11] = [
     DggrsSpec {
         id: DggrsUid::ISEA3HDGGRID,
         name: DggrsName::ISEA3H,
@@ -148,5 +148,33 @@ pub const DGGRS_SPECS: [DggrsSpec; 9] = [
         default_refinement_level: RefinementLevel::new_const(2),
         max_relative_depth: RelativeDepth::new_const(6),
         default_relative_depth: RelativeDepth::new_const(4),
+    },
+    DggrsSpec {
+        id: DggrsUid::IVEA7H,
+        name: DggrsName::IVEA7H,
+        tool: DggrsImplementation::DGGAL,
+        title: "",
+        description: "",
+        uri: "",
+        crs: "",
+        min_refinement_level: RefinementLevel::new_const(0),
+        max_refinement_level: RefinementLevel::new_const(20),
+        default_refinement_level: RefinementLevel::new_const(2),
+        max_relative_depth: RelativeDepth::new_const(5),
+        default_relative_depth: RelativeDepth::new_const(3),
+    },
+    DggrsSpec {
+        id: DggrsUid::IVEA7H_Z7,
+        name: DggrsName::IVEA7H_Z7,
+        tool: DggrsImplementation::DGGAL,
+        title: "",
+        description: "",
+        uri: "",
+        crs: "",
+        min_refinement_level: RefinementLevel::new_const(0),
+        max_refinement_level: RefinementLevel::new_const(20),
+        default_refinement_level: RefinementLevel::new_const(2),
+        max_relative_depth: RelativeDepth::new_const(5),
+        default_relative_depth: RelativeDepth::new_const(3),
     },
 ];
